5398da4ca3617105e20118bfab26b12ad4993a7f
wavilap <wavilap0509@gmail.com>
2016-07-25 14:36:00 -0500

    Create view signin

--- src/signin/template.js
+++ src/signin/template.js
@@ -1,32 +1,29 @@
 var yo = require('yo-yo');
 var landing = require('../landing');
 
-var signupForm = yo`<div class="col s12 m7">
+var signinForm = yo`<div class="col s12 m7">
   <div class="row">
     <div class="signup-box">
       <h1 class="platzigram">Platzigram</h1>
       <form action="" class="signup-form">
-        <h2>Regístrate para ver fotos de tus amigos estudiando en Platzi</h2>
         <div class="section">
           <a href="" class="btn btn-fb hide-on-small-only">Iniciar sesión con Facebook</a>
           <a href="" class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
         </div>
         <div class="divider"></div>
         <div class="section">
-          <input type="email" name="email" placeholder="Correo electrónico">
-          <input type="text" name="name" placeholder="Nombre completo">
           <input type="text" name="username" placeholder="Nombre de usuario">
           <input type="password" name="password" placeholder="Contraseña">
-          <button type="submit" class="btn waves-effect waves-light btn-signup">Regístrate</button>
+          <button type="submit" class="btn waves-effect waves-light btn-signup">Inicia sesión</button>
         </div>
       </form>
     </div>
   </div>
   <div class="row">
     <div class="login-box">
-      ¿Tienes una cuenta? <a href="/signin">Entrar</a>
+      ¿No tienes una cuenta? <a href="/signup">Registrate</a>
     </div>
   </div>
 </div>`;
 
-module.exports = landing(signupForm);
+module.exports = landing(signinForm);
